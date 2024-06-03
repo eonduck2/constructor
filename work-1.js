dbfce8f919b86870534e45d6e3efb1b6aa6fbb63
eonduck2 <whdtnxd@gmail.com>
2024-06-03 11:53:15 +0900

    update func 'workTwo' in 'work-1.js'

--- work-1.js
+++ work-1.js
@@ -3,8 +3,51 @@
  *
  * NOTE: 2. 목적은 생성자 함수를 이해하기 위함입니다.
  * @eonduck2
- * @param {number}a 첫 번째 매개변수
- * @param {number}b 두 번째 매개변수
- * @returns 객체
+ * @param {string}a 객체의 키 역할
+ * @param {string}b 객체의 벨류 역할
+ * @returns {object} 두 매개변수로 라벨링이 된 객체
  */
-const workOne = (a, b) => {};
+// FIXME
+// const workOne = (a, b) => {
+//   const localObj = {};
+
+//   localObj[a] = b;
+
+//   return localObj;
+// };
+// FIXME
+
+const workTwo = (...a) => {
+  const localObj = {};
+  const { ...rest } = a;
+
+  for (let key in rest) {
+    console.log(rest[key]);
+    if (Number(key) % 2 == 0) {
+      localObj[rest[key]] = a[1];
+    }
+  }
+  console.log(localObj);
+
+  return localObj;
+};
+
+workTwo(`테스트 키`, `테스트 벨류`, `가나다라`, `마바사아`);
+
+// console.log(workOne(`테스트 키`, `테스트 벨류`));
+
+// const pracConstructor = (testArg) => {
+//   this.test1 = testArg;
+// };
+
+// const test = new pracConstructor(`테스트`);
+
+// function User(name) {
+//   this.name = name;
+//   this.isAdmin = false;
+// }
+
+// let user = new User("지우");
+
+// alert(user.name);
+// alert(user.isAdmin);
