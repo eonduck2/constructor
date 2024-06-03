27ab60467f511a7dfa75f740e6160f78ff3cfe76
eonduck2 <whdtnxd@gmail.com>
2024-06-03 12:45:26 +0900

    source code 'work-4'

---
+++ work-4.js
@@ -0,0 +1,45 @@
+import makeMember from "./work-3.js";
+/**
+ * NOTE(Subject):
+ *
+ * NOTE(Object):
+ *
+ * NOTE(Verb):
+ *
+ * NOTE: KDT 멤버들 객체 만들기
+ */
+
+// const makeMember = require('./work-3.js');
+// console.log(makeMember(1, "김보미"));
+
+// let arrayMembers = [];
+
+// for(let i = 0; i < names.length; i++) {
+//   let member = makeMember(i, names[i])
+//   arrayMembers.push(member);
+// }
+
+const names = [
+  "구하림",
+  "김보미",
+  "김수현",
+  "김정수",
+  "문혜림",
+  "배성빈",
+  "백지원",
+  "송이현",
+  "신지윤",
+  "유으뜸",
+  "유호영",
+  "이연승",
+  "이재영",
+  "이종수",
+  "임유진",
+  "정호연",
+  "조우식",
+  "조자연",
+  "최유진",
+  "황재민",
+];
+
+makeMember(names.forEach((element) => {}));
