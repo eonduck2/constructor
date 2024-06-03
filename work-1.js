876768b7dccbecbe56229250b0bb436c419430a0
eonduck2 <whdtnxd@gmail.com>
2024-06-03 10:54:02 +0900

    update comments on 'work-1.js'

--- work-1.js
+++ work-1.js
@@ -1,4 +1,10 @@
 /**
  * NOTE: 1. 이 파일은 생성자 함수 연습 파일입니다.
+ *
  * NOTE: 2. 목적은 생성자 함수를 이해하기 위함입니다.
+ * @eonduck2
+ * @param {number}a 첫 번째 매개변수
+ * @param {number}b 두 번째 매개변수
+ * @returns 객체
  */
+const workOne = (a, b) => {};
