02cca2a17c40d575ef2d0e63558aeb409846886d
eonduck2 <whdtnxd@gmail.com>
2024-06-03 11:55:37 +0900

    init

--- work-1.js
+++ work-1.js
@@ -1,53 +1,26 @@
-/**
- * NOTE: 1. 이 파일은 생성자 함수 연습 파일입니다.
- *
- * NOTE: 2. 목적은 생성자 함수를 이해하기 위함입니다.
- * @eonduck2
- * @param {string}a 객체의 키 역할
- * @param {string}b 객체의 벨류 역할
- * @returns {object} 두 매개변수로 라벨링이 된 객체
- */
-// FIXME
-// const workOne = (a, b) => {
-//   const localObj = {};
+// * 이 파일은 기본 함수 연습 파일입니다.
+// * 목적은 함수를 조금 더 이해하려고 합니다.
+// * 매개변수가 객체인 경우가 아래에 작성될 예정입니다.
 
-//   localObj[a] = b;
+function workOne(a, b) {
+  //* logic : 매개변수 두개를 받은 다음
+  //* logic : return은 객체였음 좋겠다.
+  if (typeof a === "string") {
+    if (typeof b === "string") {
+      let localVariable = {};
 
-//   return localObj;
-// };
-// FIXME
+      localVariable[a] = a;
+      localVariable[b] = b;
 
-const workTwo = (...a) => {
-  const localObj = {};
-  const { ...rest } = a;
-
-  for (let key in rest) {
-    console.log(rest[key]);
-    if (Number(key) % 2 == 0) {
-      localObj[rest[key]] = a[1];
+      return localVariable;
+    } else {
+      console.error("좋은말할때 문자열로 바꿔라");
     }
+  } else {
+    console.error("좋은말할때 문자열로 바꿔라");
   }
-  console.log(localObj);
+  // * condition : 매개변수 모두 문자열이여야 한다.
+}
 
-  return localObj;
-};
-
-workTwo(`테스트 키`, `테스트 벨류`, `가나다라`, `마바사아`);
-
-// console.log(workOne(`테스트 키`, `테스트 벨류`));
-
-// const pracConstructor = (testArg) => {
-//   this.test1 = testArg;
-// };
-
-// const test = new pracConstructor(`테스트`);
-
-// function User(name) {
-//   this.name = name;
-//   this.isAdmin = false;
-// }
-
-// let user = new User("지우");
-
-// alert(user.name);
-// alert(user.isAdmin);
+let test = workOne(1, 2);
+console.log(test);
