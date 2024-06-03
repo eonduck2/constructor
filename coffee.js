e06b4ce86d5ed74e22412d8bd2686dd61f6cc4c5
eonduck2 <whdtnxd@gmail.com>
2024-06-03 15:41:51 +0900

    init 'coffee.js'

---
+++ coffee.js
@@ -0,0 +1,10 @@
+class MakeCoffee {
+  constructor(wondoo, water, sugar, milk) {
+    this.wondoo = wondoo;
+    this.water = water;
+    this.sugar = sugar;
+    this.milk = milk;
+  }
+}
+
+console.log(new MakeCoffee(`탄자니아`, 300, true, true));
